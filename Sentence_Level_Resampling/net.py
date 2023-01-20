7c27101f52a799c5e364e0d4a0c30b76f48dc97d
Shahad Mahmud <shahad9381@gmail.com>
2023-01-21 01:33:36 +0600

    bidirectional lstm

--- Sentence_Level_Resampling/net.py
+++ Sentence_Level_Resampling/net.py
@@ -77,7 +77,7 @@ class BertWithEmbeds(nn.Module):
         super().__init__()
         
         self.embeddings = nn.Embedding(32000, 256)
-        self.lstm = nn.LSTM(input_size=256, hidden_size=64, num_layers=2)
+        self.lstm = nn.LSTM(bidirectional=True, input_size=256, hidden_size=64, num_layers=2, dropout=0.3, batch_first=True)
         
 
         coonfig = AutoConfig.from_pretrained(
@@ -86,16 +86,23 @@ class BertWithEmbeds(nn.Module):
         #self.bert = BertModel.from_pretrained('bert-base-multilingual-cased')
         self.bert = AutoModelForPreTraining.from_pretrained(
             "csebuetnlp/banglabert", config=coonfig)
-        self.projection = nn.Linear(768, 50, False)
+        self.projection = nn.Linear(768, 64, False)
 
         self.top_rnns = top_rnns
         if top_rnns:
-            self.rnn = nn.LSTM(bidirectional=True, num_layers=4, dropout=0.5,
-                               input_size=768+64, hidden_size=832//2, batch_first=True)
+            self.rnn = nn.LSTM(bidirectional=True, num_layers=4, dropout=0.3,
+                               input_size=768+64+64+64, hidden_size=(832+64+64)//2, batch_first=True)
         self.fc = nn.Sequential(
-            nn.Linear(768+64, 512),
-            nn.Dropout(0.5),
-            nn.Linear(512, vocab_size)
+            nn.Linear(768+64+64+64, 512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, vocab_size)
         )
 
         self.device = device
@@ -124,19 +131,19 @@ class BertWithEmbeds(nn.Module):
                 enc = encoded_layers.hidden_states[-1]
 
         # create the n-grams
-        # n_gram_batch = []
-        # for sample in enc:
-        #     n_tokens = sample.shape[0]
-        #     n_grams = [torch.mean(sample[0:2], dim=0)]
-        #     for i in range(1, n_tokens-1):
-        #         n_grams.append(torch.mean(sample[i-1:i+2], dim=0))
-        #     n_grams.append(torch.mean(sample[n_tokens-1:], dim=0))
+        n_gram_batch = []
+        for sample in enc:
+            n_tokens = sample.shape[0]
+            n_grams = [torch.mean(sample[0:2], dim=0)]
+            for i in range(1, n_tokens-1):
+                n_grams.append(torch.mean(sample[i-1:i+2], dim=0))
+            n_grams.append(torch.mean(sample[n_tokens-1:], dim=0))
 
-        #     n_gram_batch.append(torch.stack(n_grams))
+            n_gram_batch.append(torch.stack(n_grams))
 
-        # n_gram_batch = torch.stack(n_gram_batch)
-        # n_gram_batch = self.projection(n_gram_batch)
-        # enc = torch.concat([enc, n_gram_batch], dim=2)
+        n_gram_batch = torch.stack(n_gram_batch)
+        n_gram_batch = self.projection(n_gram_batch)
+        enc = torch.concat([enc, n_gram_batch], dim=2)
         
         enc = torch.concat([enc, y_1], dim=2)
 
@@ -145,4 +152,5 @@ class BertWithEmbeds(nn.Module):
         logits_focal = self.fc(enc)
         y_hat = self.crf.forward(logits_focal)
         logits_crf = self.crf.loss(logits_focal, y)
+        
         return logits_focal, logits_crf, y, y_hat
