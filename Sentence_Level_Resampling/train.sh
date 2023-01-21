e5346ad15c40f149583f910f53bd82dfe1f11749
Shahad Mahmud <shahad9381@gmail.com>
2023-01-21 13:20:47 +0600

    remove experimental models

--- Sentence_Level_Resampling/train.sh
+++
@@ -1,5 +0,0 @@
-cd lexical_db_bangla
-python setup.py install
-cd ..
-python main.py --data_dir "/media/shahad/Data/Personal/codes/NLP-Hackathon/Hacathon-NER-Team-Retro/data/preprocessed" \
-        --output_dir "./banner_train_sen_sample" --num_train_epochs 30 --banner_weighted
